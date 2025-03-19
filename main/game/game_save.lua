bfabe7266ec951a9ff11f012679fa35b5cbf270f
Dylan Mines <dylan.e.mines@gmail.com>
2025-03-19 16:22:37 -0600

    Updated the way games are saved, and changed game_data and game_save to be more OO

--- main/game/game_save.lua
+++ main/game/game_save.lua
@@ -1,8 +1,58 @@
-local save = {}
+local M = {}
 
-save.time = 0
-save.size = vmath.vector3(0,0,0)
-save.template =  {}
-save.board = {}
+local state = { --private static dictionary for my java head
+    time = 0,
+    size = vmath.vector3(0),
+    template = {},
+    board = {}
+}
 
-return save
+function M.setTime(num)
+    state.time = num;
+end
+
+function M.getTime()
+    return state.time;
+end
+
+function M.setSize(num)
+    state.size = num;
+end
+
+function M.getSize()
+    return state.size;
+end
+
+function M.setTemplate(table)
+    state.template = table;
+end
+
+function M.addToTemplate(num)
+    table.insert(state.template, num)
+end
+
+function M.getTemplate()
+    return state.template;
+end
+
+function M.setBoard(table)
+    state.board = table;
+end
+
+function M.addToBoard(num)
+    table.insert(state.board, num)
+end
+
+function M.getBoard()
+    return state.board;
+end
+
+function M.setStateTable(table)
+    state = table;
+end
+
+function M.getStateTable()
+    return state;
+end
+
+return M;
--- main/game_data.lua
+++ main/game_data.lua
@@ -1,10 +1,65 @@
-local data = {}
-data.highscore = 0
-data.switch_power = 20
-data.muted = true
-data.exp = 0
-data.crystals = 5
-data.saved = false
+local M = {}
 
-return data
+local state = {
+    highscore = 0,
+    exp = 0,
+    crystals = 5,
+    saved = false,
+    switchPower = 0,
+} --all scripts share
+
+
+function M.setHighScore(score)
+    state.highscore = score;
+end
+
+function M.getHighScore()
+    return state.highscore;
+end
+
+function M.setSwitchPower(num)
+    state.switchPower = num;
+end
+
+function M.getSwitchPower()
+    return state.switchPower;
+end
+
+function M.setExp(num)
+    state.exp = num;
+end
+
+function M.getExp()
+    return state.exp;
+end
+
+function M.setCrystals(num)
+    state.crystals = num;
+end
+
+function M.getCrystals()
+    return state.crystals;
+end
+
+function M.setSaved(value)
+    print("setting saved as "..tostring(value))
+    state.saved = value;
+end
+
+function M.getSaved()
+    print("the value of saved is "..tostring(state.saved))
+    return state.saved;
+end
+
+function M.setStateTable(table)
+    for k,v in pairs(table) do
+        state[k] = table[k] or state[k] --restore default if no value is given
+    end
+end
+
+function M.getStateTable()
+    return state;
+end
+
+return M
 
